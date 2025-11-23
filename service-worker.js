aa7adc9436f04623994f994199994eb444123afd
Sulieman Alhassan <sulimanalhassan.123.as@gmail.com>
2025-11-23 10:23:39 +0000

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+service-worker.js
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('never-hide-ai-cache').then((cache) => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './style.css',
+        './script.js',
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png'
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
